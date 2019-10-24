ad1ae04ffe606879bafe462577c6d8d19076d861
Chawit.O <chawit.ocr@gmail.com>
2019-10-24 12:01:49 +0100

    setup index.js and basic backends

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const router = require('./config/router')
+const { dbURI, port } = require('./config/environment')
+
+mongoose.connect(dbURI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => console.log(`Express connected to port ${port}`)
+)
+
+app.use(bodyParser.json())
+
+app.use('/api', router)
+
+app.get('/*', (req, res) => res.status(404).json({ message: 'page not found' }))
+
+app.listen(port, () => console.log(`Running on port ${port}`))
+
+module.exports = app
+
