5d4b085540171d412ae7379cb3d13fdd6c014094
Chawit.O <chawit.ocr@gmail.com>
2019-10-24 21:31:34 +0100

    setting up basic react frontend home page

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <>
+    <h1>Hello World</h1>
+    <h2>Frip Advisor Home Page</h2>
+  </>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
