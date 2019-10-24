4555063e5f68900cc1f3951417fe7f9703a7cc4f
Simon <simon.a19954@hotmail.fr>
2019-10-24 16:32:35 +0100

    user db

--- config/router.js
+++ config/router.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 
 const users = require('../controllers/auth')
+// const secureRouter = require('../lib/secureRouter')
 
 router.route('/register')
   .post(users.register)
+
+router.route('/login')
+  .post(users.login)
 
 module.exports = router
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect(dbURI,
 
 app.use(bodyParser.json())
 
-app.use(logger)
+app.use('/api', logger)
 
 app.use('/api', router)
 
