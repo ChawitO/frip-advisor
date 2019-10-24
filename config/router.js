7357a664964a494c0dc104b5f50991c7505e0d46
Chawit.O <chawit.ocr@gmail.com>
2019-10-24 18:36:17 +0100

    refactoring the cache.get()

--- config/router.js
+++ config/router.js
@@ -1,20 +1,8 @@
 const router = require('express').Router()
 
 const users = require('../controllers/auth')
-const caches = require('../lib/cache')
-const axios = require('axios')
 
 router.route('/register')
   .post(users.register)
-
-router.route('/testCache')
-  .get(function(req, res) {
-    caches.get('https://apidojo-booking-v1.p.rapidapi.com/locations/auto-complete', { languagecode: 'en', text: 'rome' })
-      .then(info => {
-        console.log('should be last:', info)
-        res.status(200).json(info)
-      })
-      .catch(err => res.status(404).json(err))
-  })
 
 module.exports = router
