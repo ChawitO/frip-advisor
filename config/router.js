1854deb24813e6614de66c1046078f297f050b53
Chawit.O <chawit.ocr@gmail.com>
2019-10-24 21:08:49 +0100

    cities and hotel listing

--- config/router.js
+++ config/router.js
@@ -1,8 +1,15 @@
 const router = require('express').Router()
 
 const users = require('../controllers/auth')
+const bookings = require('../controllers/booking')
 
 router.route('/register')
   .post(users.register)
+
+router.route('/cities')
+  .get(bookings.index)
+
+router.route('/cities/:id/hotels')
+  .get(bookings.hotelIndex)
 
 module.exports = router
