d66774bbbb88c89e35f03cfc07130c9979212954
Olly Aulakh <oaulakh@streamamp.com>
2019-10-25 13:57:35 +0100

    navbar and base structure

---
+++ src/components/auth/Login.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import axios from 'axios'
+import Auth from '../../lib/auth'
+
+class Login extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      data: {
+        email: '',
+        password: ''
+      }
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    const data = { ...this.state.data, [e.target.name]: e.target.value }
+    this.setState({ data })
+    console.log(data)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+	
+    axios.post('/fripadvisor/login', this.state.data)
+      .then(res => {
+        Auth.setToken(res.data.token)
+        this.props.history.push('/database')
+      })
+      .catch(err => console.log(err.message))
+  }
+
+  render() {
+    return (
+    // BULMA option
+      <section className="section">
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+            <h2 className="title">Login</h2>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  className="input"
+                  name="email"
+                  placeholder="Email"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <button type="submit" className="button is-primary">Login</button>
+          </form>
+        </div>
+      </section>		
+    )
+  }
+}
+
+export default Login
---
+++ src/components/common/Home.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Home = () => <h1>Welcome to FripAdvisor!  Home of the frip of your life!</h1>
+
+export default Home 
