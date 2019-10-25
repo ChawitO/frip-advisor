b5a08749248fac574f29cd9d5fec041bf89a1d02
Chawit.O <chawit.ocr@gmail.com>
2019-10-25 16:19:00 +0100

    fixed missing destructuring { secret }

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
-const secret = require('../config/environment')
+const { secret } = require('../config/environment')
 
 function register(req, res) {
   User
@@ -16,7 +16,7 @@ function login(req, res) {
       if (!user || !user.validatePassword(req.body.password)) {
         return res.status(401).json({ message: 'Unauthorized' }) 
       }
-      const token = jwt.sign({ sub: user._id }, secret, { expiresIn: '6h' }) 
+      const token = jwt.sign({ sub: user._id }, secret, { expiresIn: '6h' })
       res.status(202).json({ message: `Welcome Back ${user.username}`, token })
     })
     .catch(() => res.status(401).json({ message: 'Unauthorized' } ))
