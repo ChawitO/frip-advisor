01d73345bb4010165d332db9f1a0fdd088eb81b9
Chawit.O <chawit.ocr@gmail.com>
2019-10-25 11:57:24 +0100

    simple listing of cities response

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,47 @@
 import React from 'react'
+import axios from 'axios'
 
-const Home = () => (
-  <h1>Frip Advisor Home Page</h1>
-)
+export default class Home extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      searchCities: '',
+      cities: null
+    }
 
-export default Home
+    this.onChange = this.onChange.bind(this)
+    this.onSearch = this.onSearch.bind(this)
+  }
+
+  onChange({ target: { name, value } }) {
+    this.setState({ [name]: value })
+  }
+
+  onSearch(e) {
+    e.preventDefault()
+    axios.get(`/api${e.target.getAttribute('api')}`, { params: { languagecode: 'en', text: this.state.searchCities } })
+      .then(res => this.setState({ cities: res.data }))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    console.log(this.state)
+    const { searchCities, cities } = this.state
+    return (
+      <section>
+        <h1>Frip Advisor Home Page</h1>
+        <form onChange={this.onChange} onSubmit={this.onSearch} api='/cities'>
+          <input name='searchCities' value={searchCities} placeholder='search here for destination city'/>
+          <button type='submit'>Search</button>
+        </form>
+        {cities && cities.map(city => (
+          <div key={city.dest_id}>
+            <img src={city.image_url} alt={`image of ${city.name}`}/>
+            <h4>{city.name}</h4>
+            <h6></h6>
+          </div>
+        ))}
+      </section>
+    )
+  }
+}
