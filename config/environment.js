aaf3ace127f5f425b4ff4ac900cf61c9e9151317
Chawit.O <chawit.ocr@gmail.com>
2019-11-15 15:23:17 +0000

    fixed: MONGODB_URL to MONGODB_URI in config/environment.js

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URL || `mongodb://localhost/frip-advisor${process.env.NODE_ENV || 'dev'}`
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/frip-advisor${process.env.NODE_ENV || 'dev'}`
 const secret = process.env.SECRET || 'le secret token'
 
 module.exports = { dbURI, port, secret }
